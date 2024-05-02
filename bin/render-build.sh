ab883c8985dca07307820452b70daba611111a4a
sezan <naimulsezan@gmail.com>
2024-05-03 02:10:03 +0600

    render04

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,11 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
 bundle install
+
+# Precompile assets
 bundle exec rails assets:precompile
+
+# Clean up assets (optional)
 bundle exec rails assets:clean
